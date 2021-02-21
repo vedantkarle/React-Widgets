f3ae9d1fea50886f72d4d6d794baaa13e4fe1e20
vedant prasad priya karle <59387428+vedantkarle@users.noreply.github.com>
2021-02-21 13:03:57 +0530

    Search And Dropdown Created

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import Accordion from "./Accordion";
+import Dropdown from "./Dropdown";
+import Search from "./Search";
 
 const items = [
   {
@@ -15,10 +18,33 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
-    <div>
-      <Accordion items={items} />
+    <div className="ui container" style={{ marginTop: "10px" }}>
+      {/* <Accordion items={items} /> */}
+      {/* <Search /> */}
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
